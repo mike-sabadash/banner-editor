01daf100ae1dbd367a6f41ba0776e66ac4c77e9b
mike-sabadash <lostvoxmusic@gmail.com>
2026-08-31 22:08:19 +0300

    Switch app to Editor Core v2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Editor from "./Editor";
-import { bootstrapProjectPersistence, installEditorRuntime } from "./editorRuntime";
+import EditorCoreV2 from "./core/EditorCoreV2";
 import "./styles.css";
-import "./enhancements.css";
 
-await bootstrapProjectPersistence();
-createRoot(document.getElementById("root")!).render(<StrictMode><Editor /></StrictMode>);
-installEditorRuntime();
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <EditorCoreV2 />
+  </StrictMode>,
+);
