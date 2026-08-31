c01673d6d840d2c792b615a267c3d1b671725d48
mike-sabadash <lostvoxmusic@gmail.com>
2026-08-31 18:38:24 +0300

    Install editor runtime fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Editor from "./Editor";
+import { bootstrapProjectPersistence, installEditorRuntime } from "./editorRuntime";
 import "./styles.css";
 import "./enhancements.css";
+
+await bootstrapProjectPersistence();
 createRoot(document.getElementById("root")!).render(<StrictMode><Editor /></StrictMode>);
+installEditorRuntime();
