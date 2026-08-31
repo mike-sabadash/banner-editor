248e8a9147a1377e6e162139259ab21863326018
mike-sabadash <lostvoxmusic@gmail.com>
2026-08-31 22:44:32 +0300

    Wire layout director API into Vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins: [react()] });
+import { layoutDirectorMiddleware } from "./server/layoutDirector";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    {
+      name: "layout-director-api",
+      configureServer(server) {
+        server.middlewares.use(layoutDirectorMiddleware());
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use(layoutDirectorMiddleware());
+      },
+    },
+  ],
+});
