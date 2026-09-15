7f648d33bf2d593b3d96e6cb116ac43e96c2d1d5
mike-sabadash <lostvoxmusic@gmail.com>
2026-09-15 20:30:29 +0300

    Restore editor controls compatibility module

---
+++ src/web-scene/editorProductControls.ts
@@ -0,0 +1,2 @@
+export {FONT_FAMILIES,FONT_STYLES,DEFAULT_CAMPAIGN_BORDER,fontStyleLabel,mergedDisplayFormats,mergeCampaignFormats} from "./editorControls";
+export type {CampaignBorder} from "./editorControls";
